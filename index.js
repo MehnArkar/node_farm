fcb919bde1619db38248b2a068c58590af677597
Arkar Min <arkarmin@Arkars-MacBook-Air.local>
2023-01-05 15:37:29 +0630

    exports module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
+const replaceTemplate = require('./modules/replaceTemplate')
 
 // const textIn=fs.readFileSync('./txt/input.txt','utf-8');
 // console.log(textIn);
@@ -9,19 +10,7 @@ const querystring = require('querystring');
 // const textOut = fs.writeFileSync('./txt/output.txt','This is what we know about avocado : ${textIn}.\nCreate on ${Date.now()}');
 // console.log('file written');
 
-const replaceTemplate = (temp, product) => {
-   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-   output = output.replace(/{%IMAGE%}/g, product.image);
-   output = output.replace(/{%PRICE%}/g, product.price);
-   output = output.replace(/{%FROM%}/g, product.from);
-   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-   output = output.replace(/{%QUANTITY%}/g, product.quantity);
-   output = output.replace(/{%DESCRIPTION%}/g, product.description);
-   output = output.replace(/{%ID%}/g, product.id);
 
-   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-   return output;
-};
 
 const tempOverview = fs.readFileSync('./templates/overview.html', 'utf-8');
 const tempCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
@@ -45,7 +34,6 @@ var server = http.createServer((req, res) => {
 
    }else  if(pathname==='/product'){
       res.writeHead(200,{'Content-type':'text/html'});
-   
       const product = dataObj[query.substring(3,query.length)];
       const output = replaceTemplate(tempProduct,product);
       res.end(output);
